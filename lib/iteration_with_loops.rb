a8fb37771631ac91a88871cba41b761f7fe11daa
Adil Gondal <adil.gondal@yale.edu>
2020-05-22 10:24:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,9 +2,9 @@ def find_min_in_nested_arrays(src)
   result =[]
   r = 0 
   size = src.length
-  min = 0
   while r < size do 
     c = 0 
+    min = src[r][c]
     while c < src[r].length do 
       if src[r][c] < min
         min = src[r][c]
